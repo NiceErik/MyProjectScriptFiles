ecdee6a9cab764272039dd8e6968570f3626d21a
NiceErik <erikngutcat@gmail.com>
2023-08-20 08:56:06 -0400

    Adding file from OG project

---
+++ Test 4.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test4 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Yes we did.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
