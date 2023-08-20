12152e9fb72e25c9d7c535afc8cfef6ec9104991
NiceErik <erikngutcat@gmail.com>
2023-08-20 08:30:44 -0400

    Removed the "Blah blah blah"

--- Test 2.cs
+++ Test 2.cs
@@ -7,8 +7,7 @@ public class Test2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-     // Meaningless drivel   
-     // blah blah blah
+     // Meaningless drivel
     }
 
     // Update is called once per frame
